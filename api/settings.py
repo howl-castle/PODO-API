b44343692d00d3a476b0bbf0ee91c699c56f2746
inviisiblejessica <eunjung@inviisible.co>
2023-03-12 21:03:50 +0900

    Feature .env

--- api/settings.py
+++ api/settings.py
@@ -15,9 +15,9 @@ import os
 from pathlib import Path
 from urllib.parse import urlparse
 import environ
-
 # from google.cloud import secretmanager
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(DEBUG=(bool, False))
 env_file = os.path.join(BASE_DIR, ".env")
@@ -26,6 +26,7 @@ if os.path.isfile(env_file):
     # Use a local secret file, if provided
     env.read_env(env_file)
 
+"""
 elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
     # Pull secrets from Secret Manager
     project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
@@ -38,29 +39,13 @@ elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
 
 else:
     raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
+"""
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'gleaming-cove-378613.ue.r.appspot.com',
-    '127.0.0.1',
-    'localhost',
-    ''
-]
-
-# SECURITY WARNING: It's recommended that you use this when
-# running in production. The URL will be known once you first deploy
-# to App Engine. This code takes the URL and converts it to both these settings formats.
 APPENGINE_URL = env("APPENGINE_URL", default=None)
 
 if APPENGINE_URL:
